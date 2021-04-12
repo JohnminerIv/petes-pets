b2d55b89677f288a137a4234e30c84c1f3d94443
JohnminerIv <john.douglas.miner.iv@gmail.com>
2021-04-12 14:21:31 -0800

    i think i Implemented Emails but i really am to lazy to check and don't feel good just want to stop doing this project and dealing with life for a good long while

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,12 @@ module.exports = (app) => {
   /* GET home page. */
   app.get('/', (req, res) => {
     const page = req.query.page || 1
+    Pet.findOne()
+      .then((pet) => {
+        console.log(pet)
+      })
     Pet.paginate({}, { page: page }).then((results) => {
+      console.log(results)
       res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
     });
   });
